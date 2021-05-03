470c3db1888b90f8413e8439cc76568132348484
majimmy88 <majimmy88@gmail.com>
2021-05-03 15:18:46 -0700

    Added component files

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="flex justify-center text-white bg-gray-800">
+      <p>Jimmy Ma, Copyright &copy; 2021</p>
+    </footer>
+  );
+};
+
+export default Footer;
