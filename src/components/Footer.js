f8b852247653773aa083cde9cd79f4e55e2478d2
majimmy88 <majimmy88@gmail.com>
2021-05-07 23:11:56 -0700

    Adjusted footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex justify-center text-white bg-gray-800">
+    <footer className="flex justify-center text-gray-400 mb-8">
       <p>Jimmy Ma, Copyright &copy; 2021</p>
     </footer>
   );
