b48ed33eb706b5242032c30045fd69db83c86447
majimmy88 <majimmy88@gmail.com>
2021-05-07 23:13:17 -0700

    Work carousel in progress

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,19 +1,93 @@
+import React, { useState } from 'react';
+import Onboarding from '../assets/onboarding-clip.gif';
+import MusicPlayer from '../assets/music-player-clip.gif';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+
+const projects = [
+  {
+    name: 'New User Onboarding - Logos News',
+    image: Onboarding,
+    imageAlt: 'Onboarding Clip',
+    description: `Based on google analytics, our writers who had a complete profile
+    had significantly more readership. This new user onboarding flow was
+    created for writers to fill in a complete profile including a bio,
+    their credentionals, and a profile picture as soon as they register
+    on our site.`,
+  },
+  {
+    name: 'Music Player',
+    image: MusicPlayer,
+    imageAlt: 'Music Player Clip',
+    description: `Music player with features such as now playing, queue, top 10,
+    search, and browse. Application optimized with caching and
+    server-side HTTP requests.`,
+  },
+  {
+    name: 'Rental Reviews Microservice',
+    image: Onboarding,
+    imageAlt: 'Rental Reviews Screenshot',
+    description: `Music player with features such as now playing, queue, top 10,
+    search, and browse. Application optimized with caching and
+    server-side HTTP requests.`,
+  },
+];
+
 const Work = () => {
+  const [current, setCurrent] = useState(0);
+  const length = projects.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(projects) || projects.length <= 0) {
+    return null;
+  }
+
   return (
-    <section>
+    <section className="h-96 mb-52">
       <header>
-        <h1 className="text-2xl text-gray-500 mb-3">Work</h1>
+        <h1 className="text-2xl text-gray-500 mb-8">Work</h1>
       </header>
-      <div className="text-gray-400">
-        <h1>New User Onboarding - Logos News</h1>
-        <div className="w-auto h-96"></div>
-        <h1>User Profile Pages - Logos News</h1>
-        <div className="w-auto h-96"></div>
-        <h1>Music Player</h1>
-        <div className="w-auto h-96"></div>
-        <h1>Travel Planner</h1>
-        <div className="w-auto h-96"></div>
-        <h1>Rental Reviews Microservice</h1>
+      <div className="text-gray-400 relative h-full">
+        <FaArrowAltCircleLeft
+          className="text-5xl absolute top-1/2 left-0 z-10 opacity-20 hover:opacity-100"
+          onClick={prevSlide}
+        />
+        <FaArrowAltCircleRight
+          className="text-5xl absolute top-1/2 right-0 z-10 opacity-20 hover:opacity-100"
+          onClick={nextSlide}
+        />
+        {projects.map((project, index) => {
+          return (
+            <div
+              className={
+                index === current
+                  ? 'opacity-100 duration-1000'
+                  : 'opacity-0 duration-1000'
+              }
+              key={index}
+            >
+              {index === current && (
+                <div>
+                  <h1 className="mb-2">{project.name}</h1>
+                  <div className="flex justify-center">
+                    <img
+                      src={project.image}
+                      alt={project.imageAlt}
+                      className="w-auto h-auto mb-8 max-h-80"
+                    />
+                  </div>
+                  <p>{project.description}</p>
+                </div>
+              )}
+            </div>
+          );
+        })}
       </div>
     </section>
   );
