2ef15c032f6cbaaddbaedde75790ed2465a828ec
Jimmy Ma <majimmy88@gmail.com>
2021-05-14 01:32:32 -0700

    Added pages

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import Header from './../components/Header';
+import About from './../components/About';
+import Technologies from './../components/Technologies';
+import WorkCarousel from './../components/WorkCarousel';
+import Footer from './../components/Footer';
+
+const Home = () => {
+  return (
+    <div className="bg-left bg-fixed bg-hero-pattern box-border p-0 m-0 flex flex-col sm:bg-left-bottom lg:flex-row lg:bg-left-top lg:bg-cover">
+      <div className="bg-transparent w-full h-72 lg:w-1/3 lg:h-screen lg:sticky top-0">
+        <Header />
+      </div>
+      <div className="bg-white w-full pt-10 px-5 lg:w-2/3 lg:px-16">
+        <About />
+        <hr className="my-8" />
+        <Technologies />
+        <hr className="my-8" />
+        <WorkCarousel />
+        <hr className="my-8" />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Onboarding.js
@@ -0,0 +1,14 @@
+import NavBar from '../components/NavBar';
+
+const Onboarding = () => {
+  return (
+    <div>
+      <NavBar />
+      <div className="hero"></div>
+      <div>Goal</div>
+      <div>Implementation</div>
+    </div>
+  );
+};
+
+export default Onboarding;
